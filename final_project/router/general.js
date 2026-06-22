58fd4ccb2d6080d615d7557811cf90d693b380aa
kritin579 <chekurikritin@gmail.com>
2026-06-22 20:59:13 +0530

    Refactor user registration and book retrieval methods

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,68 +1,97 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-public_users.post("/register", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+public_users.post("/register", (req,res) => {
+const username = req.body.username;
+const password = req.body.password;
 
-  if (!username || !password) {
-    return res.status(404).json({ message: "Unable to register user." });
-  }
+if(username && password){
+if(!isValid(username)){
+users.push({"username":username,"password":password});
+return res.status(200).json({message:"User successfully registered. Now you can login"});
+}
+return res.status(404).json({message:"User already exists!"});
+}
 
-  if (!isValid(username)) {
-    users.push({ username, password });
-    return res.status(200).json({ message: "User successfully registered. Now you can login" });
-  }
-
-  return res.status(404).json({ message: "User already exists!" });
+return res.status(404).json({message:"Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-  return res.status(200).json(books);
+return res.status(200).json(books);
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
-  return res.status(200).json(books[isbn]);
+// Get book details based on ISBN using async/await
+public_users.get('/isbn/:isbn', async function (req, res) {
+const isbn = req.params.isbn;
+
+try {
+const response = await axios.get('http://localhost:5000/');
+return res.status(200).json(response.data[isbn]);
+} catch (error) {
+return res.status(500).json({message:error.message});
+}
 });
 
-// Get book details based on author
-public_users.get('/author/:author', function (req, res) {
-  const author = req.params.author;
+// Get book details based on author using async/await
+public_users.get('/author/:author', async function (req, res) {
+const author = req.params.author;
 
-  const filteredBooks = Object.keys(books)
-    .filter(key => books[key].author === author)
-    .reduce((obj, key) => {
-      obj[key] = books[key];
-      return obj;
-    }, {});
+try {
+const response = await axios.get('http://localhost:5000/');
+const booksData = response.data;
 
-  return res.status(200).json(filteredBooks);
+```
+const filteredBooks = Object.keys(booksData)
+  .filter(key => booksData[key].author === author)
+  .reduce((obj, key) => {
+    obj[key] = booksData[key];
+    return obj;
+  }, {});
+
+return res.status(200).json(filteredBooks);
+```
+
+} catch (error) {
+return res.status(500).json({message:error.message});
+}
 });
 
-// Get all books based on title
+// Get all books based on title using Promise callback
 public_users.get('/title/:title', function (req, res) {
-  const title = req.params.title;
+const title = req.params.title;
 
-  const filteredBooks = Object.keys(books)
-    .filter(key => books[key].title === title)
+axios.get('http://localhost:5000/')
+.then(response => {
+
+```
+  const booksData = response.data;
+
+  const filteredBooks = Object.keys(booksData)
+    .filter(key => booksData[key].title === title)
     .reduce((obj, key) => {
-      obj[key] = books[key];
+      obj[key] = booksData[key];
       return obj;
     }, {});
 
-  return res.status(200).json(filteredBooks);
+  res.status(200).json(filteredBooks);
+
+})
+.catch(error => {
+  res.status(500).json({message:error.message});
+});
+```
+
 });
 
 // Get book review
 public_users.get('/review/:isbn', function (req, res) {
-  const isbn = req.params.isbn;
-  return res.status(200).json(books[isbn].reviews);
+const isbn = req.params.isbn;
+return res.status(200).json(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
